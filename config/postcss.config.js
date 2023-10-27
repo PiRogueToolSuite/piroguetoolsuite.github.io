14ad6b3a2e2c42ac2d71559c27f2c5ea3d914fff
U039b <git@0x39b.fr>
2023-10-27 18:17:50 +0200

    Fix syntax highlighting

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -30,6 +30,7 @@ module.exports = {
           './assets/scss/common/_dark.scss',
           './node_modules/bootstrap/scss/_dropdown.scss',
           './node_modules/katex/dist/katex.css',
+          './node_modules/highlight.js/scss/github-dark-dimmed.scss',
         ]),
       ],
     }),

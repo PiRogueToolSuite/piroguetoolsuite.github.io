03831dbae62409af6e5fa9fe5c186d39d9dd410f
Arky <hitmanarky@gmail.com>
2024-08-20 22:04:11 +0700

    Enable Bootstrap tooltips (Fixes #247)

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,9 @@
 // Put your custom JS code here
+
+
+// Enable Bootstrap tooltips  
+import { Tooltip } from 'bootstrap';
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+// eslint-disable-next-line no-unused-vars
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl))
